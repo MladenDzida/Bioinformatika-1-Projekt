82a3b25c510ad780880d9bf83e4a08ba2385b991
Zdravko Grgić <zdravko.grgic12@gmail.com>
2022-05-18 19:46:16 +0200

    dodane metode za racunanje hasha 1, hasha 2 i za racunanje fingerprinta

--- Cuckoo.cpp
+++ Cuckoo.cpp
@@ -3,7 +3,8 @@
 #include <winsock2.h>
 #include <windows.h>
 #include <openssl/sha.h>
-#include <math.h>
+#include <cmath>
+#include <bitset>
 
 using namespace std;
 
@@ -13,25 +14,36 @@ CuckooFilter::CuckooFilter(int number_of_buckets,int bucket_size, int fingerprin
     this-> fingerprint_size_in_bits = fingerprint_size_in_bits;
 }
 
-unsigned char *CuckooFilter::return_hash(unsigned char *to_hash){
+uint64_t CuckooFilter::return_hash(unsigned char *to_hash){
+    // define length of characters
     size_t length = strlen((char*)to_hash);
+    HashOutput output;
 
-    cout << length << endl;
-    
+    // predefine output of SHA1 algorithm
     unsigned char *results = new unsigned char[SHA_DIGEST_LENGTH];
-    SHA1(to_hash, length, results);
+    SHA1(to_hash, length, output.sha_output);
 
-    return results;
+    uint64_t ret = 0;
+    // return value of SHA1
+    ret = output.hash_64;
+
+    return ret;
 }
 
-unsigned long CuckooFilter::transform_to_long(unsigned char* to_transform, int length) {
-    unsigned long int to_return = 0;
-    
-    for (int i = 0; i < length; i++) {
-        to_return |= to_transform[i] << (i * 8);
-    }
+struct H_KEY CuckooFilter::AddrAndFingerprint(uint64_t has) {
+    H_KEY to_ret;
+    uint32_t mask = (uint32_t) (pow(2, 32)) - 1;
 
-    return to_return;
+    to_ret.h_1 = mask & (has >> 32);
+    to_ret.fingerprint = (has & mask) % ((uint32_t) (pow(2, this->fingerprint_size_in_bits) - 1));
+
+    HashOutput tmp;
+    // store fingerprint inside Union tmp in order to transform value to unsigned char.
+    tmp.hash_32 = to_ret.fingerprint;
+    // send fingerprint to hashing function, but use unsigned char representation
+    to_ret.h_2 = to_ret.h_1 ^ (uint32_t) this->return_hash(tmp.sha_output);
+
+    return to_ret;
 }
 
 
@@ -41,47 +53,20 @@ int main(){
 
     unsigned char s_2[] = "Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh Mladen sas sadfsgfs dfgfsdgsfdh Mgfhgfhgdjdg jh gfhdghdghgh dghdghdgh";
     unsigned char s_3[] = "Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh";
-
     
-    CuckooFilter filter(100000, 4, 8);
-    unsigned char posalji[] = "Idemo";
-    unsigned char *posalji_pointer = posalji;
+    CuckooFilter filter(100000, 4, 2);
+    uint64_t returned = filter.return_hash(s_2);
+    uint64_t returned_2 = filter.return_hash(s_3);
 
-    unsigned char *results = filter.return_hash(s_2);
-    unsigned char *results_2 = filter.return_hash(s_3);
+    H_KEY got = filter.AddrAndFingerprint(returned);
+    cout << got.h_1 << endl;
+    cout << got.h_2 << endl;
+    cout << got.fingerprint << endl;
 
-    unsigned long int got = filter.transform_to_long(results, 20);
-    unsigned long int got_2 = filter.transform_to_long(results_2, 20);
-
-    cout << got << endl;
-    cout << got_2 << endl;
-    // cout << *results << endl;
-
-    //int bucket = 3;
-    //const unsigned char s_1[] = "Mladei";
-    //const unsigned char s_2[] = "Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh Mladen sas sadfsgfs dfgfsdgsfdh Mgfhgfhgdjdg jh gfhdghdghgh dghdghdgh";;
-    //const unsigned char s_3[] = "Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh Mladen sas sadfsgfs dfgfsdgsfdh sgfhgfhgdjdg jh gfhdghdghgh dghdghdgh";
-
-    //unsigned char end[SHA_DIGEST_LENGTH];
-
-    //SHA1(s_1, SHA_DIGEST_LENGTH, end);
-    //unsigned long int l_1 = end[0] | (end[1] << 8) | (end[2] << 16) | (end[3] << 24);
-
-    //SHA1(s_2, SHA_DIGEST_LENGTH, end);
-    //unsigned long int l_2 = end[0] | (end[1] << 8) | (end[2] << 16) | (end[3] << 24);
-
-    //SHA1(s_3, SHA_DIGEST_LENGTH, end);
-    //unsigned long int l_3 = end[0] | (end[1] << 8) | (end[2] << 16) | (end[3] << 24);
-
-    //printf("\n");
-
-    //// cout << l_1 % bucket << endl;
-    //// cout << l_2 % bucket << endl;
-    //// cout << l_3 % bucket << endl;
-
-    //cout << l_1 << endl;
-    //cout << l_2 << endl;
-    //cout << l_3 << endl;
+    H_KEY got_2 = filter.AddrAndFingerprint(returned_2);
+    cout << got_2.h_1 << endl;
+    cout << got_2.h_2 << endl;
+    cout << got_2.fingerprint << endl;
 
     system("Pause");
 }
--- Cuckoo.h
+++ Cuckoo.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <openssl/sha.h>
+
 class CuckooFilter
 {
 public:
@@ -9,16 +11,37 @@ public:
 
   CuckooFilter(int number_of_buckets,int bucket_size, int fingerprint_size_in_bits);
 
-  unsigned char *return_hash(unsigned char *to_hash);
+  /**
+  Calculate SHA1 on unsigned char *to_hash, and returns uint64_t reprezentation of output.
+  @param to_hash - pointer to unsigned char array on which to calculate SHA1
+  @return uint64_t - result
+  */
+  uint64_t return_hash(unsigned char *to_hash);
 
-  unsigned long transform_to_long(unsigned char* to_transform, int length);
-
-  H_KEY split_h_key();
+  /**
+  Returns H_KEY structure which has 3 values: first hash (32 bits), second hash (32 bits) and fingerprint (32 bits).
+  @param hash - uint64_t hash
+  @return H_KEY - structure with values: first hash, second hash and fingerprint.
+  */
+  struct H_KEY AddrAndFingerprint(uint64_t hash);
 
 };
 
+/**
+Holder for output data. It is used to easily  transform output of SHA1 algorithm to uint32_t or uint64_t format.
+*/
+union HashOutput {
+	uint32_t hash_32;
+	uint64_t hash_64;
+	unsigned char sha_output[SHA_DIGEST_LENGTH];
+};
+
+/**
+Structure that holds result values.
+*/
 struct H_KEY
 {
-	unsigned long int h;
-	unsigned long int key;
+	uint32_t h_1;
+	uint32_t h_2;
+	uint32_t fingerprint;
 };
